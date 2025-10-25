0d7d05e28612284b998190cf97f63ebc41c4c59c
JacobZ <zhangzx.1997@163.com>
2025-10-25 23:30:09 +0900

    add modification for blink-cmp

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,34 @@
+-- Filename: ~/github/dotfiles-latest/neovim/neobean/lua/plugins/blink-cmp.lua
+-- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/blink-cmp.lua
+
+-- HACK: blink.cmp updates | Remove LuaSnip | Emoji and Dictionary Sources | Fix Jump Autosave Issue
+-- https://youtu.be/JrgfpWap_Pg
+
+-- completion plugin with support for LSPs and external sources that updates
+-- on every keystroke with minimal overhead
+
+-- https://www.lazyvim.org/extras/coding/blink
+-- https://github.com/saghen/blink.cmp
+-- Documentation site: https://cmp.saghen.dev/
+
+-- NOTE: Specify the trigger character(s) used for luasnip
+local trigger_text = ";"
+
+return {
+  "saghen/blink.cmp",
+  enabled = true,
+  -- In case there are breaking changes and you want to go back to the last
+  -- working release
+  -- https://github.com/Saghen/blink.cmp/releases
+  -- version = "v0.13.1",
+  opts = function(_, opts)
+    
+    opts.keymap = {
+      preset = "default",
+
+      ["<CR>"] = false,
+    }
+
+    return opts
+  end,
+}
